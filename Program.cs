84bca9d9519e063e0673f95dc354eb2146c64f13
this-claudio <65410959+this-claudio@users.noreply.github.com>
2020-07-15 10:06:50 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Inversao
         static void Main(string[] args)
         {
             Calcula.Somar(Criar.Complexo(real:3.0,imaginario:2.0),Criar.Complexo(angulo:0.5878,modulo:3.6,radiano:true));
-
+            cout << "Programa encerrado" << endl;
            
             
       
